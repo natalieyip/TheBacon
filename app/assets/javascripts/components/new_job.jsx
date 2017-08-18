bdb6ae7c771d1488e2f3963c23f6fbc909baee0a
Natalie Yip <yip.natalie92@gmail.com>
2017-08-17 19:02:55 -0700

    Add pay type and employmeny type to form

--- app/assets/javascripts/components/new_job.jsx
+++ app/assets/javascripts/components/new_job.jsx
@@ -19,11 +19,20 @@ var NewJob = React.createClass({
 	render(){
 		return (
 			<div> 
+				<form>
 					<input ref='title' placeholder="Title" />
 					<input ref='company' placeholder="Company" />
 					<input ref='description' placeholder="Description" />
+					<select name="employtype" >
+					  <option value="Full Time">Full Time</option>
+					  <option value="Part Time">Part Time</option>
+					  <option value="Internship">Internship</option>
+					  <option value="Contract">Contract</option>
+					</select>
+					<input type="radio" name="paytype" value="salary" /> Salary
+  				<input type="radio" name="paytype" value="hourly" /> Hourly
 					<button onClick={this.handleClick} >Submit</button>
-				
+				</form>
 			</div>
 			)
 	}
